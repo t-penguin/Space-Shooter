63851600900d1cd2e785dcf00a6908c44c73ff54
Joseph <sourpina@gmail.com>
2025-01-23 10:57:00 -0500

    Added wave spawning
    - Added lists for asteroids, enemies, and modifiers to the GameController1 script to be assigned prefabs from the inspector.
    - Added warnings in Awake for any empty prefab lists.
    - Refactored the spawning functionality to separate functions for asteroids, enemies, and modifiers.
    - Added wave spawning functionality, which currently only spawns asteroid with speeds between 4 and 8
    - Added wave text to display the current wave

--- Assets/New Scripts/GameController1.cs
+++ Assets/New Scripts/GameController1.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,17 +8,37 @@ public class GameController1 : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _waveText;
     [SerializeField] private GameObject _gameOverPanel;
     [SerializeField] private bool _isGameOver;
     [SerializeField] private int _score;
 
     [Header("Hazard Info")]
-    [SerializeField] private GameObject _hazard;
+    [SerializeField] private List<GameObject> _asteroids;
+    [SerializeField] private List<GameObject> _enemies;
+    [SerializeField] private List<GameObject> _modifiers;
     [SerializeField] private Vector2 _hazardHorizontalRange;
     [SerializeField] private float _hazardZ;
-    [SerializeField] private float _spawnInterval;
 
-    private int waveAmount = 10;
+    [Header("Wave Info")]
+    [SerializeField] private float _startDelay;
+    [SerializeField] private float _waveInterval;
+    [SerializeField] private float _spawnInterval;
+    [SerializeField] private int _waveAmount = 10;
+    [SerializeField] private int _wave;
+
+    private void Awake()
+    {
+        if (_asteroids == null || _asteroids.Count == 0)
+            Debug.LogWarning("No asteroid prefabs in list, asteroids will not be able to spawn.");
+
+        if (_enemies == null || _enemies.Count == 0)
+            Debug.LogWarning("No enemy prefabs in list, enemies will not be able to spawn.");
+
+        if (_modifiers == null || _modifiers.Count == 0)
+            Debug.LogWarning("No modifier prefabs in list, modifiers will not be able to spawn.");
+    }
+
     private void Start()
     {
         StartCoroutine(SpawnHazard());
@@ -46,15 +67,56 @@ public class GameController1 : MonoBehaviour
 
     private IEnumerator SpawnHazard()
     {
-        Quaternion spawnRotation = new Quaternion(0, 180, 0, 1);
+        // Wait a bit before starting
+        yield return new WaitForSeconds(_startDelay);
 
+        // Main spawning loop
         while (true)
         {
-            float spawnX = Random.Range(_hazardHorizontalRange.x, _hazardHorizontalRange.y);
-            Vector3 spawnPosition = new Vector3(spawnX, 0, _hazardZ);
-            Instantiate(_hazard, spawnPosition, spawnRotation);
-            yield return new WaitForSeconds(_spawnInterval);
+            if (_isGameOver)
+                yield break;
+
+            IncrementWave();
+
+            // Wave spawning loop
+            for (int i = 0; i < _waveAmount; i++)
+            {
+                float spawnX = Random.Range(_hazardHorizontalRange.x, _hazardHorizontalRange.y);
+                SpawnAsteroid(spawnX, 4, 8);
+
+                // Wait a bit in between spawns
+                yield return new WaitForSeconds(_spawnInterval);
+            }
+
+            // Wait a bit in between waves
+            yield return new WaitForSeconds(_waveInterval);
         }
+    }
+
+    private void SpawnAsteroid(float posX, float minSpeed, float maxSpeed)
+    {
+        Quaternion rotation = new Quaternion(0, 180, 0, 1);
+        GameObject asteroid = _asteroids[Random.Range(0, _asteroids.Count)];
+        asteroid = Spawn(asteroid, posX, rotation);
+        asteroid.GetComponent<Mover1>().SetSpeed(minSpeed, maxSpeed);
+    }
+
+    private void SpawnEnemy(float posX)
+    {
+        GameObject enemy = _enemies[Random.Range(0, _enemies.Count)];
+        Spawn(enemy, posX, Quaternion.identity);
+    }
+
+    private void SpawnModifier(float posX)
+    {
+        GameObject modifier = _modifiers[Random.Range(0, _modifiers.Count)];
+        Spawn(modifier, posX, Quaternion.identity);
+    }
+
+    private GameObject Spawn(GameObject prefab, float posX, Quaternion rotation)
+    {
+        Vector3 position = new Vector3(posX, 0, _hazardZ);
+        return Instantiate(prefab, position, rotation);
     }
 
     private void OnAsteroidDestroyed() => AddScore(10);
@@ -65,6 +127,12 @@ public class GameController1 : MonoBehaviour
     {
         _score += amount;
         _scoreText.text = $"Score: {_score}";
+    }
+
+    private void IncrementWave()
+    {
+        _wave++;
+        _waveText.text = $"Wave: {_wave}";
     }
 
     private void GameOver()
